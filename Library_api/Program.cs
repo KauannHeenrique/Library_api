50fe6244ba1fd7fafdf91465591c9bf318e99d67
Kauann Henrique <kauannsantos013@gmail.com>
2024-12-08 00:22:37 -0300

    Configurando cors api

--- Library_api/Program.cs
+++ Library_api/Program.cs
@@ -11,8 +11,19 @@ builder.Services.AddDbContext<LibraryContext>(options =>
 builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", policy =>
+    {
+        policy.AllowAnyOrigin()
+            .AllowAnyMethod()
+            .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
+
+app.UseCors("AllowAll");
 
 if (app.Environment.IsDevelopment())
 {
